301dae9bc5001e1c9c8b45d8c76343e42b7f5d48
SRIKAKULAPU NAVEEN <49204570+NAVEEN45973@users.noreply.github.com>
2020-04-06 07:09:51 +0530

    Update matrimonyphp (1).sql

--- matrimonyphp/matrimonyphp (1).sql
+++ matrimonyphp/matrimonyphp (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 05, 2020 at 04:43 PM
+-- Generation Time: Apr 06, 2020 at 03:39 AM
 -- Server version: 10.3.16-MariaDB
 -- PHP Version: 7.3.7
 
@@ -21,26 +21,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `matrimonyphp`
 --
-
--- --------------------------------------------------------
-
---
--- Table structure for table `images`
---
-
-CREATE TABLE `images` (
-  `id` int(11) NOT NULL,
-  `image` varchar(100) COLLATE utf8_bin NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-
---
--- Dumping data for table `images`
---
-
-INSERT INTO `images` (`id`, `image`) VALUES
-(0, 'Screenshot (29).png'),
-(0, 'Screenshot (103).png'),
-(0, 'Screenshot (103).png');
 
 -- --------------------------------------------------------
 
@@ -72,7 +52,7 @@ CREATE TABLE `users` (
   `job` varchar(100) NOT NULL,
   `image` varchar(100) NOT NULL,
   `dateofbirth` date NOT NULL,
-  `gender` varchar(5) NOT NULL,
+  `gender` varchar(7) NOT NULL,
   `userlevel` int(2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
@@ -81,9 +61,10 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `profilestat`, `username`, `password`, `email`, `mobile`, `salary`, `job`, `image`, `dateofbirth`, `gender`, `userlevel`) VALUES
-(1, 0, 'vinay', '1234', 'vinay@gmail.com', '7712345691', '45000', 'Software Engineer', 'Screenshot (101).png', '2020-04-01', 'male', 0),
+(1, 0, 'vinay', '1234', 'vinay@gmail.com', '7712345691', '45000', 'Software Engineer', 'Screenshot (101).png', '1998-04-01', 'male', 0),
 (3, 0, 'cherry', '12345', 'cherry459@gmail.com', '7730844681', '50000', 'software Engineer', 'Screenshot (103).png', '2000-06-08', 'male', 0),
-(4, 0, 'Anu', '123456', 'anu@gmail.com', '9781235463', '35000', 'teacher', 'gr1.jpg', '1999-08-12', 'femal', 0);
+(4, 0, 'Anu', '123456', 'anu@gmail.com', '9781235463', '35000', 'teacher', 'gr1.jpg', '1999-08-12', 'female', 0),
+(6, 0, 'shreya', '12345', 'shreya12@gmail.com', '7842136213', '50000', 'Doctor', 'shreya.jpg', '1998-06-18', 'female', 0);
 
 --
 -- Indexes for dumped tables
@@ -104,7 +85,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
